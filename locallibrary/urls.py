ed20e600114a72966efd43bb7f7826deec8ee878
Dmitry Tolsky <dima.tolsky@gmail.com>
2018-09-25 17:27:18 +0600

    added catalog and models

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
+
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tosky/projects/python/learning/locallibrary/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
