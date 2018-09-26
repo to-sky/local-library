feeef7c846f8b846ce7b66563c6d5cea807a76f7
Dmitry Tolsky <dmitry.tolsky@rhinoda.com>
2018-09-26 15:19:08 +0600

    added base templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_author = Author.objects.count()
+    num_genres = Genre.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instance': num_instance,
+            'num_instances_available': num_instances_available,
+            'num_author': num_author,
+            'num_genres': num_genres
+        }
+    )
